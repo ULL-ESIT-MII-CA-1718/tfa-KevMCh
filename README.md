732170da14013776f56e7990197028700997c906
Kevin <marchinkev@gmail.com>
2017-12-31 21:13:41 +0000

    README

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
+# _The CMSA daily meals_
 ## Final Project
+
+### Intro
+
+CRUD web page to manage the daily meals. This project is created with [NodeJS](https://nodejs.org/es/) and [MongoDB](https://www.mongodb.com/) as database.
+
+### Pre-install
+
+To use it you have to install the specific tools via npm (_npm install_) and the global _gulp pre-install_. Also, you have install the basic data in MongoDB, _gulp pre-install-db_. If you use _gulp pre-install_ you install the tools and the basic data.
+
+To initialited the databases you have the _gulp start_ command and to stop it _gulp stop_.
+
+### Use
+
+When you have initialited it, you have a basic login in the root, the first user created is _admin_, and the password is equal, _admin_.
+
+After passing the login, you have all the views where you can create, read, modify and delete differents objects.
+
+* Users
+* Meals
+* Menus
+* Daily meals
 
 ### Personal Data
 * *[My GitHub Page](https://kevmch.github.io/)*
--- models/pre-install-db.js
+++ models/pre-install-db.js
@@ -5,8 +5,9 @@ mongoose.Promise = global.Promise;
 // Roles in the DB
 var Rol = require('./Rol');
 
+var rolIDAdmin = new mongoose.Types.ObjectId();
 var admin = new Rol({
-  _id: new mongoose.Types.ObjectId(),
+  _id: rolIDAdmin,
   name: 'Administrador'
 });
 
@@ -35,27 +36,56 @@ var vegan = new TypeMeal({
 
 // Save
 var adminProm = admin.save(function (err) {
-  if (err) return handleError(err);
+  if (err) console.log(err);
 });
 
 var colProm = col.save(function (err) {
-  if (err) return handleError(err);
+  if (err) console.log(err);
 });
 
 var commonProm = common.save(function (err) {
-  if (err) return handleError(err);
+  if (err) console.log(err);
 });
 
 var vegetProm = veget.save(function (err) {
-  if (err) return handleError(err);
+  if (err) console.log(err);
 });
 
 var veganProm = vegan.save(function (err) {
-  if (err) return handleError(err);
+  if (err) console.log(err);
 });
 
-Promise.all([adminProm, colProm, commonProm, vegetProm, veganProm]).
-  then(result => {
+var adminUser = admin.save(function (err) {
+  if (err) console.log(err);
+});
+
+// Admin user in the DB
+var User = require('./User');
+
+var newUser = new User ({
+  _id: new mongoose.Types.ObjectId(),
+  user: 'admin',
+  password: 'admin',
+  rol: rolIDAdmin,
+});
+
+var userAdmin = new Promise(function (fulfill, reject){
+    User.createUser(newUser, function(err, user){
+      if(err) reject(err);
+
+      fulfill(user);
+    })
+});
+
+Promise.all([
+  adminProm,
+  colProm,
+  commonProm,
+  vegetProm,
+  veganProm,
+  adminUser,
+  userAdmin
+]).then(result => {
     mongoose.connection.close()
   }
 );
