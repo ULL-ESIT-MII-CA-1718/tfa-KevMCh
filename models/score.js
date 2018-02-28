ccc6d6d06cdb0a2e4a471a89225d14fe91875d9c
Kevin <marchinkev@gmail.com>
2018-02-28 12:48:40 +0000

    Fixed bugs to create a scores

--- models/score.js
+++ models/score.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var scoreSchema = Schema({
   _id: Schema.Types.ObjectId,
   points: Number,
-  meal: { type: Schema.Types.ObjectId, ref: 'Meal' }
+  meal: { type: Schema.Types.ObjectId, ref: 'Meal' },
+  user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 var Score = mongoose.model('Score', scoreSchema);
